132e4259ded1edc1f6c0e41275c08ed250a4e3a4
maximoskp <maximos@athenarc.gr>
2021-07-12 00:10:36 +0300

    ch 10 - first keras

--- ch10_ANNs.py
+++ ch10_ANNs.py
@@ -46,3 +46,62 @@ X_test = X_test/250.
 
 class_names = ['T-shirt-top', 'Trousers', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# %% classification with two layers
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation='relu'))
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+'''
+# alternative initialization
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28])
+    keras.layers.Dense(300, activation='relu')
+    keras.layers.Dense(100, activation='relu')
+    keras.layers.Dense(10, activation='softmax')
+])
+'''
+
+# %% print summary of the model
+model.summary()
+
+# %% see layers
+print(repr(model.layers))
+# get layer
+hidden1 = model.layers[1]
+print(hidden1.name)
+
+# %% access weights and biases
+weights, biases = hidden1.get_weights()
+print(repr(weights.shape))
+print(repr(biases.shape))
+
+# %% compile model
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+# %% train model
+
+history = model.fit(X_train, y_train, epochs=30,
+                    validation_data=(X_valid, y_valid))
+
+# %% plot training history
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+pd.DataFrame(history.history).plot()
+plt.grid(True)
+plt.gca().set_ylim(0,1)
+plt.show()
+
+# validation error seems to be less than training error in each epoch
+# but this happens because validation error is computed after the 
+# the training within this epoch has been completed, while training 
+# error is computed during training.
+# Check comment in p.305: training error should be shifted half epoch
