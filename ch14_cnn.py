7be81f9f638d20e331477d07aa349c0aedac11f8
maximoskp <maximos@athenarc.gr>
2021-08-05 08:08:32 +0300

    ch14 - res34

--- ch14_cnn.py
+++ ch14_cnn.py
@@ -104,3 +104,63 @@ class_names = ['T-shirt-top', 'Trousers', 'Pullover', 'Dress', 'Coat',
 
 history = model.fit( X_train, y_train, epochs=30,
                     validation_data=(X_valid, y_valid))
+
+# %% resnet-34
+
+class ResidualUnit(keras.layers.Layer):
+    def __init__( self, filters, strides=1, activation='relu', **kwargs ):
+        super().__init__(**kwargs)
+        self.activation = keras.activations.get(activation)
+        self.main_layers = [
+            keras.layers.Conv2D(filters, 3, strides=strides,
+                                padding='same', use_bias=False),
+            keras.layers.BatchNormalization(),
+            self.activation,
+            keras.layers.Conv2D(filters, 3, strides=1,
+                                padding='same', use_bias=False),
+            keras.layers.BatchNormalization()]
+        self.skip_layers = []
+        if strides > 1:
+            self.skip_layers = [
+                keras.layers.Conv2D(filters, 1, strides=strides,
+                                    padding='same',use_bias=False),
+                keras.layers.BatchNormalization()]
+    # end init
+    def call(self, inputs):
+        Z = inputs
+        for layer in self.main_layers:
+            Z = layer(Z)
+        skip_Z = inputs
+        for layer in self.skip_layers:
+            skip_Z = layer(skip_Z)
+        return self.activation( Z + skip_Z )
+    # end call
+# end ResidualUnit
+
+model = keras.models.Sequential()
+model.add( keras.layers.Conv2D(64, 7, strides=2, input_shape=[224,224,3],
+                               padding='same', use_bias=False) )
+model.add( keras.layers.BatchNormalization() )
+model.add( keras.layers.Activation('relu') )
+model.add( keras.layers.MaxPool2D(pool_size=3, strides=2, padding='same') )
+prev_filters = 64
+for filters in [64]*3 + [128]*4 + [256]*6 + [512]*3:
+    strides = 1 if filters == prev_filters else 2
+    model.add(ResidualUnit(filters, strides=strides))
+    prev_filters = filters
+model.add( keras.layers.GlobalAvgPool2D() )
+model.add( keras.layers.Flatten() )
+model.add( keras.layers.Dense(10, activation='softmax') )
+
+# %% resize
+
+X_train_res = tf.image.grayscale_to_rgb( tf.image.resize(X_train, [224,224]) )
+X_valid_res = tf.image.grayscale_to_rgb( tf.image.resize(X_valid, [224,224]) )
+
+# %% train
+
+history = model.fit( X_train_res, y_train, epochs=30,
+                    validation_data=(X_valid_res, y_valid))
+
+
+
